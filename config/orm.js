cce2f8710a8b7a8067e196174020e508a12d7035
SchneiderMan <jschneid94@gmail.com>
2018-09-10 20:49:48 -0700

    Created burger model, completed two methods

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,25 @@
 const connection = require('./orm.js');
 
 const orm = {
-    selectAll: function(tableInput) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, [tableInput], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    insertOne: function(tableInput, colToInput, valToInsert) {
+    insertOne: function(tableInput, colToInput, valToInsert, cb) {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [tableInput, colToInput, valToInsert], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    updateOne: function(tableInput, colToInput, newColVal, id, idVal) {
+    updateOne: function(tableInput, colToInput, newColVal, id, idVal, cb) {
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString, [tableInput, colToInput, newColVal, id, idVal], function(err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     }
 }
---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require('../config/orm.js');
+
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(col, val, cb) {
+        orm.insertOne("burgers", col, val, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(cb) {
+        orm.updateOne("burgers", function(res) {
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
