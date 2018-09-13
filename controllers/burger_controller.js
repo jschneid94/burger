a324805cb71f16ffb2908911613e866abc1e429a
SchneiderMan <jschneid94@gmail.com>
2018-09-12 20:05:37 -0700

    Added router listener for get.

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,6 +1,16 @@
 const express = require("express");
+var router = express.Router();
+
 var burger = require("../models/burger");
 
-var router = express.Router();
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+});
 
 module.exports = router;
