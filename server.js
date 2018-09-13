f299c6453020fd6ff9db7699ff3c45a9ec713b1a
SchneiderMan <jschneid94@gmail.com>
2018-09-13 14:11:58 -0700

    Fixed error with controller requirement

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/catController.js");
+var routes = require("./controllers/burger_controller.js");
 
 app.use(routes);
 
