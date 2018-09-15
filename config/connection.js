8d41d536179ca512d8f70aa6a1a1ffee2d44fc57
SchneiderMan <jschneid94@gmail.com>
2018-09-14 21:17:46 -0700

    Changed button colors, started Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require('mysql');
+var connection;
 
 // Create connection
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "burgers_db"
 });
+}
 
 // Connect to the database
 connection.connect(function(err) {
